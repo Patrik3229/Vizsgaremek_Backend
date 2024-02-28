c3ba0a34dc898f3580058cd24aea08310a2f161e
Fannichan <elekes.fanni2005@gmail.com>
2024-02-28 16:10:43 +0100

    deleting the unnecerry stuff

--- src/allergens/allergens.controller.ts
+++ src/allergens/allergens.controller.ts
@@ -1,16 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AllergensService } from './allergens.service';
 import { CreateAllergenDto } from './dto/create-allergen.dto';
-import { UpdateAllergenDto } from './dto/update-allergen.dto';
 
 @Controller('allergens')
 export class AllergensController {
   constructor(private readonly allergensService: AllergensService) {}
-
-  @Post()
-  create(@Body() createAllergenDto: CreateAllergenDto) {
-    return this.allergensService.create(createAllergenDto);
-  }
 
   @Get()
   findAll() {
@@ -20,15 +14,5 @@ export class AllergensController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.allergensService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAllergenDto: UpdateAllergenDto) {
-    return this.allergensService.update(+id, updateAllergenDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.allergensService.remove(+id);
   }
 }
--- src/allergens/allergens.service.ts
+++ src/allergens/allergens.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAllergenDto } from './dto/create-allergen.dto';
-import { UpdateAllergenDto } from './dto/update-allergen.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AllergensService {
-  create(createAllergenDto: CreateAllergenDto) {
-    return 'This action adds a new allergen';
-  }
-
+  constructor(private readonly db : PrismaClient) {}
+  
   findAll() {
     return `This action returns all allergens`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} allergen`;
-  }
-
-  update(id: number, updateAllergenDto: UpdateAllergenDto) {
-    return `This action updates a #${id} allergen`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} allergen`;
   }
 }
