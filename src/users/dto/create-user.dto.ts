64401ed5c65abc0054c6919a8b6285ef2b961644
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2024-02-28 12:23:01 +0100

    Completed user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString } from "class-validator"
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  email: string
+
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @IsString()
+  @IsNotEmpty()
+  password: string
+
+  @IsString()
+  @IsNotEmpty()
+  role: string
+}
