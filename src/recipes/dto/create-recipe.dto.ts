6e2f22d2116edf205bfc46e0d64977ab39cd4ca5
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2024-02-28 13:03:03 +0100

    Completed recipe DTO & typo fix

--- src/recipes/dto/create-recipe.dto.ts
+++ src/recipes/dto/create-recipe.dto.ts
@@ -1,1 +1,26 @@
-export class CreateRecipeDto {}
+import { IsDateString, IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateRecipeDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string
+
+  @IsString()
+  description: string
+
+  @IsString()
+  @IsNotEmpty()
+  content: string
+
+  @IsNumber()
+  @IsNotEmpty()
+  preptime: number
+
+  @IsDateString()
+  @IsNotEmpty()
+  posted: string
+
+  @IsInt()
+  @IsNotEmpty()
+  user_id: number
+}
