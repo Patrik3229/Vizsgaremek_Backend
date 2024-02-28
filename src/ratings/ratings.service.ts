0895011100963308ef0e2a04880548c6650eb859
Fannichan <elekes.fanni2005@gmail.com>
2024-02-28 16:27:15 +0100

    initial

--- src/ratings/ratings.service.ts
+++ src/ratings/ratings.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class RatingsService {
+  constructor(private readonly db : PrismaService) {}
+
   create(createRatingDto: CreateRatingDto) {
-    return 'This action adds a new rating';
+    return this.db.ratings.create({
+      data : {
+        content : createRatingDto.content,
+        rating : createRatingDto.rating,
+        recipe_id : createRatingDto.recipe_id,
+        user_id : createRatingDto.user_id
+      }
+    });
   }
 
-  findAll() {
-    return `This action returns all ratings`;
+  findAllByUser(user_id : number) {
+    return this.db.ratings.findMany({
+      where : {user_id}
+    });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} rating`;
+    return this.db.ratings.findUnique({
+      where : {id}
+    });
   }
 
   update(id: number, updateRatingDto: UpdateRatingDto) {
-    return `This action updates a #${id} rating`;
+    return this.db.ratings.update({
+      where : {id},
+      data : {
+        content : updateRatingDto.content,
+        rating : updateRatingDto.rating,
+        posted : Date.now.toString()
+      }
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} rating`;
+    return this.db.ratings.delete({
+      where : {id}
+    });
   }
 }
