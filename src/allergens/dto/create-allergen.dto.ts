25b12a62d48e158af7ae55c405b481d543b146fd
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2024-02-28 12:25:22 +0100

    completed allergen DTO

--- src/allergens/dto/create-allergen.dto.ts
+++ src/allergens/dto/create-allergen.dto.ts
@@ -1,6 +1,10 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateAllergenDto {
+  @IsNumber()
+  @IsNotEmpty()
+  id: number
+  
   @IsString()
   @IsNotEmpty()
   name: string
