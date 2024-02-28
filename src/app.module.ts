2379baac3e93a649a1526669dc0a72528a7b4474
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2024-02-28 12:17:11 +0100

    Recipes DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, RecipesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
