b250b277a772abc7a7d5ac8b7f0a27bae1676921
Fannichan <elekes.fanni2005@gmail.com>
2024-02-29 13:42:53 +0100

    something

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,18 +8,34 @@ import * as argon2 from "argon2";
 export class UsersService {
   constructor(private readonly db: PrismaService) {}
 
+  /**
+   * email szerint kereső
+   * @param email 
+   * @returns user adatait
+   */
   findByEmail(email: string) {
     return this.db.users.findUnique({
       where: { email }
     })
   }
 
-  //password hashing
+  /**
+   * jelszó titkosítás
+   * @param password 
+   * @returns hash jelszót
+   */
   async passwordHash(password : string) {
+    /**megadott password hash-eli */
     const secret = await argon2.hash(password)
     return secret
   }
   
+  /**
+   * új User add hozzá
+   * @param createUserDto 
+   * @param secret 
+   * @returns úgy felhasználót
+   */
   create(createUserDto: CreateUserDto, secret : string) {
     return this.db.users.create({
       data : {
