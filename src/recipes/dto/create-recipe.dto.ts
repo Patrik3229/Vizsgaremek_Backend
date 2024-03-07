a488e3cfd0ffa3454ca0eeb895b59b2e9b9dc51b
Fannichan <elekes.fanni2005@gmail.com>
2024-03-07 14:45:07 +0100

    recipes dto

--- src/recipes/dto/create-recipe.dto.ts
+++ src/recipes/dto/create-recipe.dto.ts
@@ -1,25 +1,37 @@
-import { IsDateString, IsEmpty, IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsDateString, IsEmpty, IsInt, IsNotEmpty, IsNumber, IsString, MaxLength, Min } from "class-validator";
 
+/**
+ * recipes dto, create
+ */
 export class CreateRecipeDto {
+  /**a recept címe */
   @IsString()
   @IsNotEmpty()
+  @MaxLength(100)
   title: string
 
+  /**a recepthez tartozó rövid leírás */
   @IsString()
+  @MaxLength(300)
   description: string
 
+  /**a recept leírása */
   @IsString()
   @IsNotEmpty()
   content: string
 
+  /**a recept elkészítéséhez tartozó idő percben*/
   @IsNumber()
   @IsNotEmpty()
+  @Min(1)
   preptime: number
 
+  /**timestamp arról hogy mikor postolták a receptet */
   @IsDateString()
   @IsEmpty()
   posted: string
 
+  /**postoló id-ja */
   @IsInt()
   @IsNotEmpty()
   user_id: number
