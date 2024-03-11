b013efbc2748c7d5f2a898084a7f7f3a13678981
Fannichan <elekes.fanni2005@gmail.com>
2024-03-11 15:37:53 +0100

    dis javítása

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -48,7 +48,7 @@ export class RecipesService {
   }
 
   /**
-   * 
+   * --------------
    * @param id a recept id-ja
    * @param updateRecipeDto a modosítandó adatok 
    * @returns 
@@ -80,8 +80,8 @@ export class RecipesService {
     }
     const stringSql = `'%${string}%'`
     if(array.length == 0){
-      return this.db.$queryRaw`SELECT id, title, description, preptime, posted, AVG(ratings.rating) AS rating FROM recipes INNER JOIN recipe_allergens ON recipes.id = recipe_id INNER JOIN allergens ON recipe_allergens.allergen_id = allergens.id INNER JOIN ratings ON recipes.id = ratings.recipes_id WHERE title LIKE ${stringSql} AND description LIKE ${stringSql}` 
+      return this.db.$queryRaw`SELECT id, title, description, preptime, posted, AVG(ratings.rating) AS rating FROM recipes INNER JOIN recipe_allergens ON recipes.id = recipe_id INNER JOIN allergens ON recipe_allergens.allergen_id = allergens.id INNER JOIN ratings ON recipes.id = ratings.recipes_id WHERE title LIKE ${stringSql} OR description LIKE ${stringSql}` 
     }
-    return this.db.$queryRaw`SELECT id, title, description, preptime, posted, AVG(ratings.rating) AS rating FROM recipes INNER JOIN recipe_allergens ON recipes.id = recipe_id INNER JOIN allergens ON recipe_allergens.allergen_id = allergens.id INNER JOIN ratings ON recipes.id = ratings.recipes_id WHERE title LIKE ${stringSql} AND description LIKE ${stringSql} AND allergens.id NOT IN ${array}`
+    return this.db.$queryRaw`SELECT id, title, description, preptime, posted, AVG(ratings.rating) AS rating FROM recipes INNER JOIN recipe_allergens ON recipes.id = recipe_id INNER JOIN allergens ON recipe_allergens.allergen_id = allergens.id INNER JOIN ratings ON recipes.id = ratings.recipes_id WHERE title LIKE ${stringSql} OR description LIKE ${stringSql} AND allergens.id NOT IN ${array}`
   }
 }
